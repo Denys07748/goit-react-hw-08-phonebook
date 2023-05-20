9d7479ecd0c3ee971dfba5d87407103b0a5297e3
Denys07748 <denis07748@gmail.com>
2023-05-20 12:34:48 +0300

    remove IconButton

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,14 +1,10 @@
-// import Button from '@mui/material/Button';
-import AuthNav from 'components/AuthNav/AuthNav';
+ import AuthNav from 'components/AuthNav/AuthNav';
 import { Navigation } from 'components/Navigation/Navigation';
 import UserMenu from 'components/UserMenu/UserMenu';
 import { useSelector } from 'react-redux';
 import { selectIsLoggedIn } from 'redux/auth/selectors';
 // import { Header } from './Header.styled';
-import { AppBar, Box, Container, IconButton, Toolbar } from '@mui/material';
-// import MenuIcon from '@mui/icons-material';
-
-// const useStyles = makeStyles(theme);
+import { AppBar, Box, Container, Toolbar } from '@mui/material';
 
 const Header = () => {
   const isLoggedIn = useSelector(selectIsLoggedIn);
@@ -20,9 +16,6 @@ const Header = () => {
           <Box sx={{ flexGrow: 1 }}>
             <Navigation />
           </Box>
-          <IconButton edge="start" color="inherit" aria-label="menu">
-            {/* <MenuIcon /> */}
-          </IconButton>
           {isLoggedIn ? <UserMenu /> : <AuthNav />}
         </Toolbar>
       </Container>
