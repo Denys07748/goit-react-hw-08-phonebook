c65cc0088521f8cc5e0a9612c43be840bb909020
Denys07748 <denis07748@gmail.com>
2023-05-20 13:11:27 +0300

    update node

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,8 @@
- import AuthNav from 'components/AuthNav/AuthNav';
+import AuthNav from 'components/AuthNav/AuthNav';
 import { Navigation } from 'components/Navigation/Navigation';
 import UserMenu from 'components/UserMenu/UserMenu';
 import { useSelector } from 'react-redux';
 import { selectIsLoggedIn } from 'redux/auth/selectors';
-// import { Header } from './Header.styled';
 import { AppBar, Box, Container, Toolbar } from '@mui/material';
 
 const Header = () => {
