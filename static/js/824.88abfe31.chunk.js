e1996066cf137c65beba008ac057d94e98791d45
Denys07748 <95140883+Denys07748@users.noreply.github.com>
2023-10-24 16:42:49 +0000

    Deploying to gh-pages from @ Denys07748/goit-react-hw-08-phonebook@ff101de4c2bc2dbdf4be862c726d8cb776fbf03e 🚀

---
+++ static/js/824.88abfe31.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[824],{3322:function(e,t,n){n.r(t),n.d(t,{default:function(){return u}});var c=n(1087),r=n(1614),a=n.p+"static/media/not_found_image.87813c4e1c0034d279db.png",s=n(2611),o=n(184),u=function(){return(0,o.jsxs)(r.Z,{sx:s.nC.My,children:[(0,o.jsx)("img",{src:a,alt:"Not found page"}),(0,o.jsx)(c.OL,{to:"/",style:s.LI.qF,children:"BACK TO HOME"})]})}}}]);
+//# sourceMappingURL=824.88abfe31.chunk.js.map
